7daf3df19a3bde7b3e35928581fcf96cc2766e13
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-17 20:42:44 +0530

    Initial Commit

---
+++ Drawing App/Drawing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_AppApp.swift
+//  Drawing App
+//
+//  Created by Shubham on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
